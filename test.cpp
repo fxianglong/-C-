3275c488249fafa969d5a8dfa129ab587e5f7899
Jack feng <48426950+fxianglong@users.noreply.github.com>
2019-11-21 23:29:08 +0800

    11.21练习
    11.21练习

--- test.cpp
+++ test.cpp
@@ -1,54 +1,72 @@
-#include<iostream>
-#include<string>
-using namespace std;
-//class Person
+//#include<iostream>
+//using namespace std;
+//int Isleap(int y)
 //{
-//public:
-//	string _name; // 姓名
-//};
-//class Student : public Person
-//{
-//protected:
-//	int _num; //学号
-//};
-//class Teacher 
-//{
-//protected:
-//	int _id; // 职工编号
-//};
-//class Assistant : public Student, public Teacher
-//{
-//protected:
-//	string _majorCourse; // 主修课程
-//};
-
+//	if (((y % 4==0) && (y % 100!=0)) || (y % 400==0))
+//		return 1;//闰年
+//	else return 0;//平年
+//}
 //int main()
 //{
-//	Assistant a;
-//	/*a.Person::_name = "peter";*/
-//
-//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
-//	a.Student::_name = "xxx";
-//	a.Teacher::_name = "yyy";
-//	a.Teacher::_name = "swq";
+//	int year = 0, month = 1, day = 1; int num = 0;
+//	int m_day[] = {0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//	cin >> year >> month >> day;
+//	for (int i = 0; i < month; i++)
+//		num += m_day[i];
+//	num += day;
+//	if (Isleap(year) && month>2)
+//		num += 1;
+//	cout << num << endl;
 //	system("pause");
 //	return 0;
 //}
 
-class Person {
-public:
-	virtual ~Person() { cout << "~Person()" << endl; }
-};
-class Student : public Person {
-public:
-		virtual ~Student() { cout << "~Student()" << endl; }
-};
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int year = 0, month = 1, day = 1;
+//	int m_day[] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+//	while (cin >> year >> month >> day){
+//		int num = m_day[month - 1] + day;
+//		if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) && month>2)
+//			num += 1;
+//		cout << num << endl;
+//		system("pause");
+//		return 0;
+//	}
+//}
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int n, a[1000];
+int DFS(int step, int sum, int mul)
+{
+	int r = 0;     
+	for (int i = step + 1; i<n; i++)     
+	{ 
+		int S = sum + a[i];         
+		int M = mul*a[i];        
+		if (S > M)            
+			r += 1 + DFS(i, S, M);         
+		else if (a[i] == 1)             
+			r += DFS(i, S, M);         
+		else             
+			break;         
+		while (i<n - 1 && a[i] == a[i + 1])             
+			i++;
+	}     
+	return r;
+}
+
 int main()
 {
-	Person* p1 = new Person;
-	Person* p2 = new Student;
-	delete p1;
-	delete p2;
-	system("pause");
+	cin >> n;     
+	for (int i = 0; i<n; i++)         
+		cin >> a[i];     
+	sort(a, a + n);     
+	cout << DFS(0, 1, 1) << endl;     
 	return 0;
 }
